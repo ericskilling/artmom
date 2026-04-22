560e1e55fdee63f19503046a28dc678ffaa15a79
ericskilling <ericskilling@gmail.com>
2026-04-21 20:43:49 -0600

    Configure TinaCMS Cloud with skip-cloud-checks for local dev

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ url: 'http://localhost:4001/graphql', token: 'undefined', queries,  });
+export const client = createClient({ url: 'http://localhost:4001/graphql', token: '23f7ad68a607e635c6a318ae80d99e4a806d170b', queries,  });
 export default client;
   
