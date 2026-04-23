c666aacf8f793731e779f1cfb5070c857dab1742
ericskilling <ericskilling@gmail.com>
2026-04-23 13:07:51 -0600

    Update Tina config and schema

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ cacheDir: '/Volumes/HD3/Documents/Eric/code/artmom/tina/__generated__/.cache/1776970076314', url: 'https://content.tinajs.io/2.2/content/a8320455-ba98-4570-a11d-3a3eb34db3c8/github/main', token: '23f7ad68a607e635c6a318ae80d99e4a806d170b', queries,  });
+export const client = createClient({ cacheDir: '/Volumes/HD3/Documents/Eric/code/artmom/tina/__generated__/.cache/1776971244170', url: 'https://content.tinajs.io/2.2/content/a8320455-ba98-4570-a11d-3a3eb34db3c8/github/main', token: '23f7ad68a607e635c6a318ae80d99e4a806d170b', queries,  });
 export default client;
   
--- tina/__generated__/config.prebuild.jsx
+++ tina/__generated__/config.prebuild.jsx
@@ -11,7 +11,7 @@ var config_default = defineConfig({
   },
   media: {
     tina: {
-      mediaRoot: "",
+      mediaRoot: "images",
       publicFolder: "public"
     }
   },
